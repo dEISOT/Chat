398cb7baedf7a61f38da0afd0bf55625bbda3666
deisot <danielstudentbsu@gmail.com>
2023-10-09 01:12:32 +0300

    implement MessageRepository

---
+++ ChatData/Repositories/Interfaces/IMessageRepository.cs
@@ -0,0 +1,10 @@
+﻿using ChatData.Entities;
+
+namespace ChatData.Repositories.Interfaces
+{
+    public interface IMessageRepository
+    {
+        public Task<IEnumerable<Message>> GetConversationMessages(Guid conversationId);
+        public Task AddMessageAsync(Message message);
+    }
+}
---
+++ ChatData/Repositories/MessageRepository.cs
@@ -0,0 +1,28 @@
+﻿using ChatData.Contexts;
+using ChatData.Entities;
+using ChatData.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatData.Repositories
+{
+    public class MessageRepository : IMessageRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MessageRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task AddMessageAsync(Message message)
+        {
+            _db.Messages.Add(message);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Message>> GetConversationMessages(Guid conversationId)
+        {
+            return await _db.Messages.Where(m => m.ConverstaionId == conversationId).ToListAsync();
+        }
+    }
+}
